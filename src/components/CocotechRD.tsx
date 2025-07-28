617768a223a1dcd10c36725ed5d78ac875759825
Bamigboye Enoch <bamigboyeayomide095@gmail.com>
2025-07-28 04:21:21 -0700

    Update CocotechRD.tsx

--- src/components/CocotechRD.tsx
+++ src/components/CocotechRD.tsx
@@ -82,7 +82,7 @@ export function CocotechRD() {
             </div>
 
             <div className="flex justify-center lg:justify-start">
-              <button className="bg-green-700 text-white px-4 md:px-5 py-2 rounded-full text-sm font-semibold hover:bg-green-800 transition">
+              <button className="bg-green-700 text-white px-6 md:px-8 py-3  rounded-full text-sm font-semibold hover:bg-green-800 transition">
                 Learn More
               </button>
             </div>
