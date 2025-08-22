e1d2451e2fee01715eeb16b99f77af74ae4afd59
Bamigboye Enoch <bamigboyeayomide095@gmail.com>
2025-08-22 17:38:21 +0100

    Update waitlist-proxy.js

--- netlify/functions/waitlist-proxy.js
+++ netlify/functions/waitlist-proxy.js
@@ -1,16 +1,24 @@
-export default async (req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
+export async function handler(event, context) {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ error: 'Method not allowed' }),
+    };
   }
+
+  const body = JSON.parse(event.body);
 
   const response = await fetch(
     'https://script.google.com/macros/s/AKfycbwm5CW4QZagvFaF2XVnY53nm5n9S9oI2LqzNI8vwfxCsWHuZYttkfMI6C7WAP48QXHqFg/exec',
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(req.body),
+      body: JSON.stringify(body),
     }
   );
   const data = await response.text();
-  res.status(200).send(data);
-};
+  return {
+    statusCode: 200,
+    body: data,
+  };
+}
