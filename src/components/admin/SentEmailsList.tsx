063d1a9401f01fabcf1c989e722e5e7e5daf4418
Bamigboye Enoch <98586631+Baydee@users.noreply.github.com>
2026-06-18 17:26:31 +0100

    Update SentEmailsList.tsx

--- src/components/admin/SentEmailsList.tsx
+++ src/components/admin/SentEmailsList.tsx
@@ -294,9 +294,9 @@ export const SentEmailsList: React.FC<SentEmailsListProps> = ({ isLoading: initi
             </div>
           </div>
         ) : (
-          <div className="flex-1 flex flex-col">
+          <div className="flex-1 flex flex-col min-h-0">
             {/* Email Items */}
-            <div className="flex-1 overflow-y-auto">
+            <div className="flex-1 overflow-y-auto min-h-0">
               {emails.map(email => (
                 <div
                   key={email.id}
